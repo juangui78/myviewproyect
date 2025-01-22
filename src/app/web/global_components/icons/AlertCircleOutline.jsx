013d8fd6afcfe8ce7492fae2dd04c829cb04b321
devSteb2002 <layyagami9@gmail.com>
2025-01-21 19:41:20 -0500

    inicio del feed

---
+++ src/app/web/global_components/icons/AlertCircleOutline.jsx
@@ -0,0 +1,18 @@
+
+
+export const AlertCircleOutline = (props) => {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="1.7em"
+        height="1.7em"
+        {...props}
+      >
+        <path
+          fill="currentColor"
+          d="M11 15h2v2h-2zm0-8h2v6h-2zm1-5C6.47 2 2 6.5 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2m0 18a8 8 0 0 1-8-8a8 8 0 0 1 8-8a8 8 0 0 1 8 8a8 8 0 0 1-8 8"
+        ></path>
+      </svg>
+    )
+  }
