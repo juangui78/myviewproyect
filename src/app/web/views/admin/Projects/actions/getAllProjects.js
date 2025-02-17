61209705f066cdf2e63acbba5a6503ac0232dbd2
devSteb2002 <layyagami9@gmail.com>
2025-02-17 17:49:25 -0500

    proyectos segun inmobiliaria

---
+++ src/app/web/views/admin/Projects/actions/getAllProjects.js
@@ -0,0 +1,29 @@
+"use server"
+import project from "@/api/models/proyect";
+import { dbConnected } from "@/api/libs/mongoose";
+import { decrypt } from "@/api/libs/crypto";
+
+dbConnected();
+
+export async function getAllProjects(id) {
+    try {
+        const idDecrypted = decrypt(id);
+        const projects = await project.find({ idCompany: idDecrypted }, { idCompany: 0,  creation_date: 0 }).lean();
+        const plainProjects = projects.map(proj => ({
+            ...proj,
+            _id: proj._id.toString(),
+        }));
+
+        return {
+            success: true,
+            data: plainProjects
+        }
+
+    } catch (error) {
+        console.log(error)
+        return {
+            message: "Error en el servidor.",
+            success: false
+        }
+    }
+}
