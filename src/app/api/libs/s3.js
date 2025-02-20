a22838bacbb159469a00b01ba76d85fcbffadb72
devSteb2002 <layyagami9@gmail.com>
2025-02-20 11:47:17 -0500

    solucion real s3

--- src/app/api/libs/s3.js
+++ src/app/api/libs/s3.js
@@ -1,37 +1,37 @@
-import { S3Client, PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+// import { S3Client, PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 
-const S3Client_ = new S3Client({
-  region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
-})
+// const S3Client_ = new S3Client({
+//   region: process.env.AWS_REGION,
+//   credentials: {
+//     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+//   },
+// })
 
-export const putObject = async (idProject, idModel, fileType) => {
+// export const putObject = async (idProject, idModel, fileType) => {
 
-  try {
+//   try {
 
-      //structure  idPRojec/idModel/ => model/scene.gltf
-                                            //scene.bin
-                                            //textures
-                                  //=> files/ writes.pdf
-                                          // etc 
+//       //structure  idPRojec/idModel/ => model/scene.gltf
+//                                             //scene.bin
+//                                             //textures
+//                                   //=> files/ writes.pdf
+//                                           // etc 
 
-      const key = `${idProject}/${idModel}/model`;
+//       const key = `${idProject}/${idModel}/model`;
 
-      const command = new PutObjectCommand({
-        Bucket: process.env.AWS_BUCKET_NAME,
-        key: key,
-        ContentType: fileType
-      })
+//       const command = new PutObjectCommand({
+//         Bucket: process.env.AWS_BUCKET_NAME,
+//         key: key,
+//         ContentType: fileType
+//       })
 
-      const response = await S3Client_.send(command);
-      return response;
+//       const response = await S3Client_.send(command);
+//       return response;
 
 
-  } catch (error) {
-    console.log(error)
-  }
+//   } catch (error) {
+//     console.log(error)
+//   }
 
-}
+// }
