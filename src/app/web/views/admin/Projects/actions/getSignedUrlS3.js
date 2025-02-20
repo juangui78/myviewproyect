3c2e1ad441386463da7f0ca26024850671e89ad4
devSteb2002 <layyagami9@gmail.com>
2025-02-20 09:52:25 -0500

    solucion error s3client

--- src/app/web/views/admin/Projects/actions/getSignedUrlS3.js
+++ src/app/web/views/admin/Projects/actions/getSignedUrlS3.js
@@ -1,5 +1,5 @@
-import { S3Client, UploadPartCommand } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+// import { S3Client, UploadPartCommand } from "@aws-sdk/client-s3";
+// import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 export async function getSignedUrlFromS3() {
     
