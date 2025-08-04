ea72f15571c5e18d74300c7165555ec6801d39af
polilotas123 <layyagami9@gmail.com>
2025-08-04 14:13:13 -0500

    solucion carga imagenes prueba 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    transpilePackages: ['three']
+    transpilePackages: ['three'],
+    images: {
+        domains: ["d7qkkiy9wjm6s.cloudfront.net"]
+    }
 };
 
 export default nextConfig;
