e49bd308e664cc84a3cb2baf2507e9f48399dbd4
Juan Parra <juangui902@gmail.com>
2025-09-03 01:02:40 -0500

    css responsive

--- src/app/web/views/user/feed/components/Cards.jsx
+++ src/app/web/views/user/feed/components/Cards.jsx
@@ -60,7 +60,7 @@ export default function Cards({ proyects }) {
               <Card
   isFooterBlurred
   key={item._id}
-  className="relative flex flex-col col-span-3 backdrop-blur-[40px] border-1 border-solid border-white h-[35vh] w-full rounded-lg max-[526px]:h-[38vh] max-[1535px]:col-span-3 max-[1218px]:grid-cols-1 max-[1218px]:w-[80%] max-[742px]:w-[95%] max-[624px]:w-[98%]"
+  className="relative flex flex-col col-span-3 backdrop-blur-[40px] border-1 border-solid border-white h-[35vh] w-full rounded-lg max-[526px]:h-[45vh] max-[1535px]:col-span-3 max-[1218px]:grid-cols-1 max-[1218px]:w-[80%] max-[742px]:w-[95%] max-[624px]:w-[98%]"
 >
   <CardBody className="h-[100%] flex flex-col md:flex-row justify-center items-center">
     {/* Imagen */}
